24b66058e4b9fd2e4e3afc0ef18013170a0eaec0
Harshit Khanna <harshitkhanna3254@gmail.com>
2023-07-27 23:14:37 -0500

    Rice University: COMP 530 Database

---
+++ LOAD.sql
@@ -0,0 +1,10 @@
+LOAD lineitem FROM TPC-H/lineitem.tbl;
+LOAD orders FROM TPC-H/orders.tbl;
+
+LOAD part FROM TPC-H/part.tbl;
+LOAD partsupp FROM TPC-H/partsupp.tbl;
+LOAD supplier FROM TPC-H/supplier.tbl;
+LOAD nation FROM TPC-H/nation.tbl;
+LOAD region FROM TPC-H/region.tbl;
+
+LOAD customer FROM TPC-H/customer.tbl;
